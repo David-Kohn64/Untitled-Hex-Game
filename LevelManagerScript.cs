61042edb64ca1c708d2fbfad7ae529ffacf667f9
David-Kohn <dkohn3@binghamton.edu>
2024-12-21 21:11:46 -0500

    Early progress - Blue tile chain-collapse built

---
+++ LevelManagerScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManagerScript : MonoBehaviour
+{
+    public static LevelManagerScript Instance {get; private set;}
+    public int currentLevel = 1; //Maybe make 0 if responsible for main menu?
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void setupLevel(int currentLevel)
+    {
+        switch (currentLevel)
+        {
+            case 1:
+
+
+            break;
+            case 2:
+
+            break;
+            case 3:
+
+            break;
+            default:
+                Debug.LogWarning("Level not implemented.");
+                break;
+        }
+    }
+}
